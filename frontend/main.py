f15f8fb9ade037f8346849f8377f1c231992a2fb
Yonatan Diga <digayona@post.bgu.ac.il>
2024-06-04 16:55:23 +0300

    added songs

--- frontend/main.py
+++ frontend/main.py
@@ -1,10 +1,6 @@
 # main.py
-from frontend.OutOfTune import OutOfTune
 from frontend.SpotifyUI import SpotifyUI
 
 if __name__ == "__main__":
-
-    #oot = OutOfTune("Roni")
-    #oot.getSongData("Menagev lah et Hadmaot.wav", True, oot)
     app = SpotifyUI()
     app.mainloop()
